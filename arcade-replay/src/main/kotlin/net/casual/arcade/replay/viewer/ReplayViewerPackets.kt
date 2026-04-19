5178e4290ad7cf2408754d09d9a84ea955bf056e
libreh <beverlydlashley@protonmail.com>
2026-04-19 21:16:31 +0200

    feat: Re-add ReplayViewer hooks
    Restores useItemHandler, onReadyHandler, onStopHandler, viewerGameMode,
    and sendToViewer on ReplayViewer for downstream consumers that drive
    viewer UI from outside arcade-replay. Whitelists container packets in
    ReplayViewerPackets so the hotbar/menu round-trip works while viewing.

--- arcade-replay/src/main/kotlin/net/casual/arcade/replay/viewer/ReplayViewerPackets.kt
+++ arcade-replay/src/main/kotlin/net/casual/arcade/replay/viewer/ReplayViewerPackets.kt
@@ -7,13 +7,27 @@ package net.casual.arcade.replay.viewer
 import net.minecraft.network.protocol.Packet
 import net.minecraft.network.protocol.common.ClientboundKeepAlivePacket
 import net.minecraft.network.protocol.common.ServerboundKeepAlivePacket
+import net.minecraft.network.protocol.game.ClientboundContainerClosePacket
+import net.minecraft.network.protocol.game.ClientboundContainerSetContentPacket
+import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket
+import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket
+import net.minecraft.network.protocol.game.ServerboundContainerClickPacket
+import net.minecraft.network.protocol.game.ServerboundContainerClosePacket
+import net.minecraft.network.protocol.game.ServerboundSetCarriedItemPacket
 
 public object ReplayViewerPackets {
     private val ALLOWED_SERVERBOUND: Set<Class<out Packet<*>>> = setOf(
-        ServerboundKeepAlivePacket::class.java
+        ServerboundKeepAlivePacket::class.java,
+        ServerboundSetCarriedItemPacket::class.java,
+        ServerboundContainerClickPacket::class.java,
+        ServerboundContainerClosePacket::class.java
     )
     private val ALLOWED_CLIENTBOUND: Set<Class<out Packet<*>>> = setOf(
-        ClientboundKeepAlivePacket::class.java
+        ClientboundKeepAlivePacket::class.java,
+        ClientboundContainerSetContentPacket::class.java,
+        ClientboundContainerSetSlotPacket::class.java,
+        ClientboundOpenScreenPacket::class.java,
+        ClientboundContainerClosePacket::class.java
     )
 
     @JvmStatic
