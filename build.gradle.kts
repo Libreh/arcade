22c5caab5c40b9c2a0cfc017dc467335e0a264e4
libreh <beverlydlashley@protonmail.com>
2026-04-22 17:45:32 +0200

    build: Bump to 0.9.0-beta.28-libreh.1, target Java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     java
 }
 
-val modVersion = "0.9.0-beta.28"
+val modVersion = "0.9.0-beta.28-libreh.1"
 
 allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
@@ -54,6 +54,10 @@ allprojects {
 
     kotlin {
         explicitApi()
+    }
+
+    tasks.withType<JavaCompile> {
+        options.release.set(25)
     }
 
     tasks {
